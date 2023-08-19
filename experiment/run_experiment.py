6c5c80028146aab7361706de871c4d5165538c85
Anne <annewmarch@gmail.com>
2023-08-20 10:14:40 +1200

    updated comments

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,6 +1,6 @@
 from adversarial_pattern_generator import AdversarialPatternGenerator
 
 accessory_type = 'glasses' ## get from args
-img_dir = './adversarial_training_imgs/'
+img_dir = './adversarial_training.db' # soemthing like this, change when we have actual db
 
 adv_pattern_generator = AdversarialPatternGenerator(accessory_type, img_dir) # and can specify any other paramenters from args
