575f6effe565226e14f17392b33337ad7d3c76c4
Ben Williams <bsw3@st-andrews.ac.uk>
2023-08-20 12:27:28 +1200

    Added argparse to run_experiment.py - a couple of TODOs to fill in for arg purposes

--- experiment/adversarial_pattern_generator.py
+++ experiment/adversarial_pattern_generator.py
@@ -15,14 +15,14 @@ Yellowish: (220, 210, 50)
 
 class AdversarialPatternGenerator:
 
-    def __init__(self, accessory_type, classification, images_dir, num_images=1, step_size=20, lambda_tv=3, probability_coeff=5, momentum_coeff=0.4, gauss_filtering=0, max_iter=15, channels_to_fix=[], stop_prob=0.01, horizontal_move=4, vertical_move=4, rotational_move=4, verbose=True):
+    def __init__(self, accessory_type, classification, images_dir, num_images=1, step_size=20, lambda_tv=3, printability_coeff=5, momentum_coeff=0.4, gauss_filtering=0, max_iter=15, channels_to_fix=[], stop_prob=0.01, horizontal_move=4, vertical_move=4, rotational_move=4, verbose=True):
         self.accessory_type = accessory_type
         self.classification = classification # what type of classification is being dodged - 'gender', 'age', 'ethnicity', 'emotion' (to do: emotion requires further preprocessing)
         self.images_dir = images_dir
         self.num_images = num_images
         self.step_size = step_size
         self.lambda_tv = lambda_tv
-        self.probability_coeff = probability_coeff
+        self.printability_coeff = printability_coeff
         self.momentum_coeff = momentum_coeff
         self.gauss_filtering = gauss_filtering
         self.max_iter = max_iter
--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,6 +1,92 @@
+import argparse
 from adversarial_pattern_generator import AdversarialPatternGenerator
 
-accessory_type = 'glasses' ## get from args
-img_dir = './adversarial_training.db' # soemthing like this, change when we have actual db
+def parse_args():
+    parser = argparse.ArgumentParser(description='Runt the experiment')
 
-adv_pattern_generator = AdversarialPatternGenerator(accessory_type, img_dir) # and can specify any other paramenters from args
+    parser.add_argument("-a", "--accessory_type",
+                        required = True,
+                        type = str,
+                        help = "Which accessory is being used in the used data"
+    )
+    parser.add_argument("-c", "--classification",
+                        required = True,
+                        choices = ["age", "emotion", "ethnicity", "gender"],
+                        type = str,
+                        help = "Which classification of the model is being dodged"
+    )
+    parser.add_argument("-d", "--images-dir",
+                        required = True,
+                        type = str,
+                        help = "Source directory of the images, either directory or .db file"
+    )
+    parser.add_argument("-n", "--num-images",
+                        default = 1,
+                        type = int,
+                        help = "How many images are being processed"
+    )
+    parser.add_argument("-s", "--step-size",
+                        default = 20,
+                        type = int,
+                        help = "The step size used in the optimisation algorithm"
+    )
+    parser.add_argument("-l", "--lambda-tv",
+                        default = 3,
+                        type = float,
+                        help = "Size of changes in total variation"
+    )
+    parser.add_argument("-p", "--printability-coeff",
+                        default = 5,
+                        type = float,
+                        help = "" # TODO: Where is this used and for what
+    )
+    parser.add_argument("-m", "--momentum-coeff",
+                        default = 0.4,
+                        type = float,
+                        help = "" # TODO: Where is this used and for what
+    )
+    parser.add_argument("-g", "--gauss-filtering",
+                        default = 0,
+                        type = float,
+                        help = "" # TODO: Where is this used and for what
+    )
+    parser.add_argument("-i", "--max-iterations",
+                        default = 15,
+                        type = int,
+                        help = "Maximum number of iterations during perturbtion" 
+    )
+    parser.add_argument("-f", "--channels-to-fix",
+                        default = [],
+                        type = list,
+                        help = "Which RGB channels to fix"
+    )
+    parser.add_argument("-P", "--stop-probability",
+                        default = 0.01,
+                        type = float,
+                        help = "The probability at which to stop iterating (if hit before max_iter)"
+    )
+    parser.add_argument("-H", "--horizontal-move",
+                        default = 4,
+                        type = int,
+                        help = "Horizontal move distance"
+    )
+    parser.add_argument("-V", "--vertical-move",
+                        default = 4,
+                        type = int,
+                        help = "Vertical movement distance")
+    parser.add_argument("-R", "--rotational-move",
+                        default = 4,
+                        type = int,
+                        help = "Rotational movement distance")
+    parser.add_argument("-v", "--v",
+                        default = True,
+                        type = bool,
+                        help = "Verbosity of experiment"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+args = parse_args()
+adv_pattern_generator = AdversarialPatternGenerator(args.accessory_type, args.img_dir) # and can specify any other paramenters from args
